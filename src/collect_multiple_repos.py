5a0b6a6ef212db6db7d45b7f6deea3970b04f50a
Yegane Jahanbani <ygn.jhi8@gmail.com>
2026-04-27 19:45:18 +0330

    feat: collect commits from multiple repositories using PyDriller

---
+++ src/collect_multiple_repos.py
@@ -0,0 +1,37 @@
+from pydriller import Repository
+import pandas as pd
+import os
+
+repos=["F:/repos/pydriller",
+    "F:/repos/flask"
+]
+
+all_commits=[]
+
+for repo_path in repos:
+
+    repo_name = os.path.basename(repo_path)
+    print(f"Processing repository: {repo_name}")
+
+    count = 0
+
+    for commit in Repository(repo_path).traverse_commits():
+
+        if count >= 50:
+            break
+
+        commit_data = {
+            "repo_name": repo_name,
+            "commit_hash": commit.hash,
+            "message": commit.msg,
+            "date": commit.committer_date,
+            "author": commit.author.name,
+            "files_changed_count": commit.files  
+        }
+
+        all_commits.append(commit_data)
+        count += 1
+df=pd.DataFrame(all_commits)
+df.to_csv("data/multi_repo_commits.csv", index=False)
+print("✅ Multi-repository dataset saved!")
+print(f"Total commits collected: {len(df)}")
